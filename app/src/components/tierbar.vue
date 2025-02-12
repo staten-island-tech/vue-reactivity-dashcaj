3dc3280b2435585e577ba631b13fef0db79376d8
dashcaj <dustinc7@nycstudents.net>
2025-02-11 21:53:06 -0500

    k

---
+++ app/src/components/tierbar.vue
@@ -0,0 +1,9 @@
+<template>
+<p>{{ t }}</p>
+
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(["t"])
+</script>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,6 +3,7 @@
   <tiersection :word="word1"/>
   <tiersection :word="word2"/>
   <tiersection :word="word3"/>
+  <tierbar :t="tee"/>
   <tierdata :tierdata="test" />
 </template>
 
@@ -10,9 +11,12 @@
   import thetitle from '@/components/thetitle.vue';
   import tiersection from '@/components/tiersection.vue';
   import tierdata from '@/components/tierchart.vue'
+  import tierbar from '@/components/tierbar.vue';
   const word1 = "word1"
   const word2 = "word2"
   const word3 = "word3"
+  const tee = "tee"
+
 
   import { defineProps } from 'vue';
   const props = defineProps(["tierdata"])
